b24defc082620e9f0183dba050be6bacffb9dba7
Abhishek7828737102 <abhishek0786ji@gmail.com>
2026-03-19 17:36:48 +0530

    Added if-else and switch case practice

---
+++ if-else.js
@@ -0,0 +1,8 @@
+let age = 16
+
+if(age>=10 && age<=20){
+  console.log("you are able for this field")
+}
+else{
+  console.log("you are enble for this field ")
+}
---
+++ switchcase.js
@@ -0,0 +1,14 @@
+let company = "rupe10";
+
+switch(company){
+    case "rupe5":
+        console.log("five rupees in pen");
+        break;
+    case "rupe10":
+        console.log("ten rupees in pen");
+        break;
+     default:
+        console.log("i dont have a pen");
+        break;
+
+}
